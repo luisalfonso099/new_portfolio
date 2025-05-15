76568dec38924480ee1c84d1472bfdd0ed79727c
Luis Alfonso <luisalfonso099@gmail.com>
2025-05-15 15:32:14 -0300

    images

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
           dataProjects.map(project => (
             <a key={project.image} className="cursor-pointer md:w-1/2 " href={project.url} target="_blank" rel="noreferrer">
               <div className="card-project rounded-lg md:max-h-72 h-auto md:min-h-52 flex justify-center p-5 w-full relative" style={{ background: project.color }}>
-                <img className="w-full" src={`../assets/${project.image}`} alt="" />
+                <img className="w-full h-40" src={`../assets/${project.image}`} alt="" />
                 <p className="text-left text-xs p-4 absolute top-0 h-full opacity-0 transition-all ease-in hover:opacity-95 bg-black/90 rounded-lg">{project.description}</p>
               </div>
               <div className="link-card-project">
