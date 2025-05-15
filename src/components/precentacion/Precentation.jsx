43e624dd88933e88aef0c311cae45cfd4cb49988
Luis Alfonso <luisalfonso099@gmail.com>
2025-05-15 15:24:32 -0300

    fix styles

--- src/components/precentacion/Precentation.jsx
+++ src/components/precentacion/Precentation.jsx
@@ -12,18 +12,17 @@ const Precentation = () => {
             <CardPerfil />
             <div className="content-text-precentation p-7">
                 <h1 className={`sm:text-4xl text-3xl lg:text-6xl leading-normal text-center lg:text-left ${darkTheme && ' text-white'}`}>
-                    Hola, soy <span className="text-[--color-primario]">Luis Laguado</span>,<br className=""/>
+                    Hola, soy <span className="text-[--color-primario] ">Luis Laguado</span>,<br className=""/>
                     Front-end Developer.
                 </h1>
                 <div className={`flex justify-center sm:justify-start items-center gap-4 mt-20 flex-wrap ${darkTheme ?  'text-white font-thin' : 'font-extralight '}`} >
-                    <span className="text-4xl sm:text-6xl font-semibold">+3</span>
+                    <span className="text-4xl sm:text-6xl font-semibold">+4</span>
                     <p className="text-sm sm:text-xl">Años <br />de experiencia</p>
                     {/* <button className={`mx-10 btn-dwonload border-${darkTheme ? 'dark text-white' : 'ligth'}`}>Dwonload C.V</button> */}
                 </div>
                 <div className={`mt-14 ${darkTheme ?  'text-white font-thin' : 'font-extralight '}`}>
-                    <p className="sm:text-xl leading-7">Cuento con experiencia en una variedad de tecnologías front-end. Mi objetivo principal es construir aplicaciones web que mejoren y transformen la experiencia del usuario.</p>
-                    <p className="sm:text-xl leading-7 my-7">Estoy feliz de trabajar en lo que me gusta, y eso se refleja en mi trabajo. La responsabilidad y el compromiso son dos valores que me han ayudado a alcanzar mis metas y continuar evolucionando como profesional.</p>
-                    <p className="sm:text-xl leading-7">¡Ten un buen dia!</p>                
+                    <p className="sm:text-xl leading-7">Soy desarrollador front-end con experiencia en una variedad de tecnologías modernas. Me especializo en crear aplicaciones web de alta calidad, con enfoque pixel perfect y completamente adaptables a cualquier dispositivo.</p>
+                    <p className="sm:text-xl leading-7 my-7">Me siento afortunado de trabajar en lo que me apasiona, y eso se refleja en la calidad de mi trabajo. La responsabilidad y el compromiso son pilares fundamentales en mi desarrollo profesional, y han sido clave para alcanzar mis metas y seguir creciendo continuamente.</p>
                 </div>
                 <Experience />
                 <Skills />
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,23 +2,22 @@ import useTheme from "../../hooks/useTheme";
 import dataProjects from '../../data/projects.json'
 import flechaDark from '../../assets/images/derecha-dark.png'
 import flecha from '../../assets/images/derecha-ligth.png'
+import './projects.css'
 const Projects = () => {
   const { darkTheme } = useTheme();
 
   return (
     <section className={"projects-container rounded-md " + (darkTheme ? 'text-white' : '')} id="portfolio">
       <h2 className="text-3xl">PORTFOLIO</h2>
-      
-<p>Aquí puedes explorar algunos de los proyectos en los que he trabajado desde su concepción hasta su finalización, aplicando las mejores prácticas de desarrollo y asegurando diseños completamente adaptables tanto para dispositivos móviles como de escritorio.</p>
+      <p>Aquí puedes explorar algunos de los proyectos en los que he trabajado desde su concepción hasta su finalización, aplicando las mejores prácticas de desarrollo y asegurando diseños completamente adaptables tanto para dispositivos móviles como de escritorio.</p>
       <div className="border-bottom-ligth"></div>
       <div className="lg:flex gap-5 text-center mt-12">
         {
           dataProjects.map(project => (
             <a key={project.image} className="cursor-pointer md:w-1/2 " href={project.url} target="_blank" rel="noreferrer">
-              <div className="card-project" >
-                <div className="rounded-lg md:max-h-72 sm:h-52 h-auto md:min-h-52 flex justify-center p-5 w-full" style={{ background: project.color }}>
-                  <img className="sm:w-8/12 w-full" src={`../assets/${project.image}`} alt="" />
-                </div>
+              <div className="card-project rounded-lg md:max-h-72 h-auto md:min-h-52 flex justify-center p-5 w-full relative" style={{ background: project.color }}>
+                <img className="w-full" src={`../assets/${project.image}`} alt="" />
+                <p className="text-left text-xs p-4 absolute top-0 h-full opacity-0 transition-all ease-in hover:opacity-95 bg-black/90 rounded-lg">{project.description}</p>
               </div>
               <div className="link-card-project">
                 <h3 className={darkTheme ? 'text-white' : ''}>{project.name}</h3>
